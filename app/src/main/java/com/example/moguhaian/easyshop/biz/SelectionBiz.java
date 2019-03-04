0341ab499a23fb1c548f33450fd1eb0138ca6131
moguhaian <499976081@qq.com>
2019-03-05 00:45:01 +0800

    fix

--- app/src/main/java/com/example/moguhaian/easyshop/biz/SelectionBiz.java
+++ app/src/main/java/com/example/moguhaian/easyshop/biz/SelectionBiz.java
@@ -3,11 +3,14 @@ package com.example.moguhaian.easyshop.biz;
 import android.webkit.WebView;
 
 import com.example.moguhaian.easyshop.Base.BaseBiz;
+import com.example.moguhaian.easyshop.Utils.JsUtils;
 
 public class SelectionBiz extends BaseBiz {
 
     public void dropDown(WebView webView) {
 //        widget-multi-dropdown
-        webView.loadUrl("findElementsByClassName(\"" + "multi-dropdown J_MutliSelect" + "\")");
+//        webView.loadUrl(JsUtils.addJsMethod("jsCangkuGoNextPage();"));
+        webView.loadUrl(JsUtils.addJsMethod("findElementsByClassName(\"" + "multi-dropdown J_MutliSelect" + "\")"));
+//        webView.loadUrl("findElementsByClassName(\"" + "multi-dropdown J_MutliSelect" + "\")");
     }
 }
