20e9dc3793de89aed8289fc93aeb4d4a866ab2a8
moguhaian <499976081@qq.com>
2019-04-14 02:00:06 +0800

    x5 fix

--- app/src/main/java/com/example/moguhaian/easyshop/fragment/Ali1688Fragment.java
+++ app/src/main/java/com/example/moguhaian/easyshop/fragment/Ali1688Fragment.java
@@ -1,32 +1,29 @@
 package com.example.moguhaian.easyshop.fragment;
 
 import android.graphics.PixelFormat;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import android.view.WindowManager;
 
 import com.example.moguhaian.easyshop.Base.BaseFragment;
 import com.example.moguhaian.easyshop.R;
 import com.example.moguhaian.easyshop.View.Ali1688Vu;
-import com.example.moguhaian.easyshop.X5.X5WebView;
 import com.example.moguhaian.easyshop.biz.Ali1688Biz;
-import com.tencent.smtt.export.external.interfaces.IX5WebChromeClient;
-import com.tencent.smtt.export.external.interfaces.JsResult;
-import com.tencent.smtt.sdk.CookieSyncManager;
-import com.tencent.smtt.sdk.DownloadListener;
-import com.tencent.smtt.sdk.WebChromeClient;
-import com.tencent.smtt.sdk.WebSettings;
-import com.tencent.smtt.sdk.WebView;
-import com.tencent.smtt.sdk.WebViewClient;
-import com.tencent.smtt.utils.TbsLog;
+import com.example.moguhaian.easyshop.weidge.MyX5WebView;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class Ali1688Fragment extends BaseFragment<Ali1688Vu, Ali1688Biz> {
 
-    @BindView(R.id.fl_webView)
-    FrameLayout flWebView;
-    private X5WebView mWebView;
+
+    @BindView(R.id.wv_x5)
+    MyX5WebView wvX5;
+    Unbinder unbinder;
     private String[] items = {"1688"};
 
 
@@ -39,136 +36,24 @@ public class Ali1688Fragment extends BaseFragment<Ali1688Vu, Ali1688Biz> {
     protected void afterOnCreate() {
         getActivity().getWindow().setFormat(PixelFormat.TRANSLUCENT);
         try {
-            if (Integer.parseInt(android.os.Build.VERSION.SDK) >= 11) {
+            if (Integer.parseInt(Build.VERSION.SDK) >= 11) {
                 getActivity().getWindow().setFlags(
-                        android.view.WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
-                        android.view.WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
+                        WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+                        WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
             }
         } catch (Exception e) {
         }
 
         setDataStrs(items);
 
-        init();
-
-//        mWebView = new X5WebView(getActivity(), null);
-//        flWebView.addView(mWebView, new FrameLayout.LayoutParams(
-//                FrameLayout.LayoutParams.MATCH_PARENT,
-//                FrameLayout.LayoutParams.MATCH_PARENT));
 
     }
 
-
-    private void init() {
-
-        mWebView = new X5WebView(getActivity(), null);
-
-        flWebView.addView(mWebView, new FrameLayout.LayoutParams(
-                FrameLayout.LayoutParams.FILL_PARENT,
-                FrameLayout.LayoutParams.FILL_PARENT));
-
-
-        mWebView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                return false;
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-
-            }
-        });
-
-        mWebView.setWebChromeClient(new WebChromeClient() {
-
-            @Override
-            public boolean onJsConfirm(WebView arg0, String arg1, String arg2,
-                                       JsResult arg3) {
-                return super.onJsConfirm(arg0, arg1, arg2, arg3);
-            }
-
-            View myVideoView;
-            View myNormalView;
-            IX5WebChromeClient.CustomViewCallback callback;
-
-            // /////////////////////////////////////////////////////////
-            //
-            /**
-             * 全屏播放配置
-             */
-            @Override
-            public void onShowCustomView(View view,
-                                         IX5WebChromeClient.CustomViewCallback customViewCallback) {
-                callback = customViewCallback;
-            }
-
-            @Override
-            public void onHideCustomView() {
-                if (callback != null) {
-                    callback.onCustomViewHidden();
-                    callback = null;
-                }
-                if (myVideoView != null) {
-                    ViewGroup viewGroup = (ViewGroup) myVideoView.getParent();
-                    viewGroup.removeView(myVideoView);
-                    viewGroup.addView(myNormalView);
-                }
-            }
-
-            @Override
-            public boolean onJsAlert(WebView arg0, String arg1, String arg2,
-                                     JsResult arg3) {
-                /**
-                 * 这里写入你自定义的window alert
-                 */
-                return super.onJsAlert(null, arg1, arg2, arg3);
-            }
-        });
-
-        mWebView.setDownloadListener(new DownloadListener() {
-
-            @Override
-            public void onDownloadStart(String arg0, String arg1, String arg2,
-                                        String arg3, long arg4) {
-            }
-        });
-
-        WebSettings webSetting = mWebView.getSettings();
-        webSetting.setAllowFileAccess(true);
-        webSetting.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
-        webSetting.setSupportZoom(true);
-        webSetting.setBuiltInZoomControls(true);
-        webSetting.setUseWideViewPort(true);
-        webSetting.setSupportMultipleWindows(false);
-        // webSetting.setLoadWithOverviewMode(true);
-        webSetting.setAppCacheEnabled(true);
-        // webSetting.setDatabaseEnabled(true);
-        webSetting.setDomStorageEnabled(true);
-        webSetting.setJavaScriptEnabled(true);
-        webSetting.setGeolocationEnabled(true);
-        webSetting.setAppCacheMaxSize(Long.MAX_VALUE);
-//        webSetting.setAppCachePath(this.getDir("appcache", 0).getPath());
-//        webSetting.setDatabasePath(this.getDir("databases", 0).getPath());
-//        webSetting.setGeolocationDatabasePath(this.getDir("geolocation", 0)
-//                .getPath());
-        // webSetting.setPageCacheCapacity(IX5WebSettings.DEFAULT_CACHE_CAPACITY);
-        webSetting.setPluginState(WebSettings.PluginState.ON_DEMAND);
-        // webSetting.setRenderPriority(WebSettings.RenderPriority.HIGH);
-        // webSetting.setPreFectch(true);
-        long time = System.currentTimeMillis();
-        TbsLog.d("time-cost", "cost time: "
-                + (System.currentTimeMillis() - time));
-        CookieSyncManager.createInstance(getActivity());
-        CookieSyncManager.getInstance().sync();
-
-    }
 
     @Override
     public void fragmentRightClick(int position) {
         super.fragmentRightClick(position);
-        mWebView.loadUrl("https://www.1688.com/");
+        wvX5.loadUrl("https://www.1688.com/");
 
     }
 
@@ -182,4 +67,17 @@ public class Ali1688Fragment extends BaseFragment<Ali1688Vu, Ali1688Biz> {
         return Ali1688Biz.class;
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
